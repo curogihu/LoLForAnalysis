2f91becebfb2c90ddb32df56715caa4c273291d8
curogihu <curoas@yahoo.co.jp>
2019-07-30 08:17:31 +0900

    refactoring algorithm
    not work to_csv method

--- how_to_go_up_bronze/extract_broken_building.py
+++ how_to_go_up_bronze/extract_broken_building.py
@@ -5,11 +5,15 @@ import pandas as pd
 
 json_path = 'C:\\output\\game\\timeline\\202077381.json'
 
+df = pd.DataFrame(index=[], columns=['gameId', 'teamId', 'laneType', 'buildingType', 'towerType', 'timestamp'])
+
 with open(json_path, 'r') as f_timelines:
     timelines = json.load(f_timelines)
 
-    output_log = {}
-    tmp = []
+    # # # output_log = {}
+    # # # tmp = []
+
+    x = 0
 
     for frame in timelines['frames']:
         if not frame['events']:
@@ -21,113 +25,11 @@ with open(json_path, 'r') as f_timelines:
                 # output_log['team_id'] = event['teamId']
                 # output_log['timestamp'] = math.floor(event['timestamp'] / 1000)
 
-                if event['teamId'] not in output_log:
-                    output_log[event['teamId']] = {}
+                print(x)
+                x = x + 1
+ 
+                df = df.append(pd.Series([202077381, event['teamId'], event['laneType'], event['buildingType'], event['towerType'], event['timestamp']], index=df.columns), ignore_index=True)
 
-                # output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                if event['laneType'] == 'BOT_LANE' and event['towerType'] == 'OUTER_TURRET':
-                    output_log[event['teamId']]['bot_1st_tower'] = 1
-                    output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                elif event['laneType'] == 'TOP_LANE' and event['towerType'] == 'OUTER_TURRET':
-                    output_log[event['teamId']]['top_1st_tower'] = 1
-                    output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                elif event['laneType'] == 'MID_LANE' and event['towerType'] == 'OUTER_TURRET':
-                    output_log[event['teamId']]['mid_1st_tower'] = 1
-                    output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                elif event['laneType'] == 'BOT_LANE' and event['towerType'] == 'INNER_TURRET':
-                    output_log[event['teamId']]['bot_2nd_tower'] = 1
-                    output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                elif event['laneType'] == 'TOP_LANE' and event['towerType'] == 'INNER_TURRET':
-                    output_log[event['teamId']]['top_2nd_tower'] = 1
-                    output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                elif event['laneType'] == 'MID_LANE' and event['towerType'] == 'INNER_TURRET':
-                    output_log[event['teamId']]['mid_2nd_tower'] = 1
-                    output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                elif event['laneType'] == 'BOT_LANE' and event['towerType'] == 'BASE_TURRET':
-                    if 'bot_inhi_tower' not in output_log:
-                        output_log[event['teamId']]['bot_inhi_tower'] = 1
-                        output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                    else:
-                        output_log[event['teamId']]['bot_inhi_tower'] += 1
-                        output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                elif event['laneType'] == 'TOP_LANE' and event['towerType'] == 'BASE_TURRET':
-                    if 'top_inhi_tower' not in output_log:
-                        output_log[event['teamId']]['top_inhi_tower'] = 1
-                        output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                    else:
-                        output_log[event['teamId']]['top_inhi_tower'] += 1
-                        output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                elif event['laneType'] == 'MID_LANE' and event['towerType'] == 'BASE_TURRET':
-                    if 'mid_inhi_tower' not in output_log:
-                        output_log[event['teamId']]['mid_inhi_tower'] = 1
-                        output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                    else:
-                        output_log[event['teamId']]['mid_inhi_tower'] += 1
-                        output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                elif event['laneType'] == 'BOT_LANE' and event['buildingType'] == 'INHIBITOR_BUILDING' and event['towerType'] == 'UNDEFINED_TURRET':
-                    if 'bot_inhi' not in output_log:
-                        output_log[event['teamId']]['bot_inhi'] = 1
-                        output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                    else:
-                        output_log[event['teamId']]['bot_inhi'] += 1
-                        output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                elif event['laneType'] == 'TOP_LANE' and event['buildingType'] == 'INHIBITOR_BUILDING' and event['towerType'] == 'UNDEFINED_TURRET':
-                    if 'top_inhi' not in output_log:
-                        output_log[event['teamId']]['top_inhi'] = 1
-                        output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                    else:
-                        output_log[event['teamId']]['top_inhi'] += 1
-                        output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                elif event['laneType'] == 'MID_LANE' and event['buildingType'] == 'INHIBITOR_BUILDING' and event['towerType'] == 'UNDEFINED_TURRET':
-                    if 'mid_inhi' not in output_log:
-                        output_log[event['teamId']]['mid_inhi'] = 1
-                        output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                    else:
-                        output_log[event['teamId']]['mid_inhi'] += 1
-                        output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                elif event['laneType'] == 'MID_LANE' and event['towerType'] == 'NEXUS_TURRET':
-                    output_log[event['teamId']]['nexus_tower'] = 1
-                    output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-                elif event['laneType'] == 'MID_LANE' and event['towerType'] == 'UNDEFINED_TURRET':
-                    output_log[event['teamId']]['nexus'] = 1
-                    output_log[event['teamId']]['timestamp'] = event['timestamp']
-
-    tmp.append(output_log)
-
-    print(tmp)
-
-    tmp_df_100 = pd.DataFrame(output_log[100].items())
-    tmp_df_100['team_id'] = 100
-    tmp_df_100['game_id'] = 202077381
-    # tmp_df_100.to_csv('test2.csv', mode='a', index=False, header=False)
-
-    tmp_df_200 = pd.DataFrame(output_log[200].items())
-    tmp_df_200['team_id'] = 200
-    tmp_df_200['game_id'] = 202077381
-    # tmp_df_200.to_csv('test2.csv', mode='a', index=False, header=False)
-
-    df = pd.concat([tmp_df_100, tmp_df_200])
-    # df.columns = ['build_type', 'break_amount', 'team_id', 'game_id']
-    df.to_csv('test3.csv',index=False, header=True)
-
-    
+print(df)
+df.to_csv('ajfkajka.csv')
+# df.to_csv('test3.csv',index=False, header=True)
